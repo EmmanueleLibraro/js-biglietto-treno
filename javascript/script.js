e01301ca1ad3c3ae9d69d98c90203719c4dfe208
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-03-19 15:24:41 +0100

    somma biglietto

--- javascript/script.js
+++ javascript/script.js
@@ -3,12 +3,15 @@
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo a schermo in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
+
+
+var costoBiglietto = 0.21;
+var sommaChilometri = 80;
+
+
 var biglietto = parseInt (prompt('Chilometri da percorrere') );
 console.log(biglietto);
 console.log( typeof(biglietto) );
 
-var costoBiglietto = 0.21;
-
-var sommaChilometri = 10;
 var chilometriFuturi = sommaChilometri * costoBiglietto;
 console.log(chilometriFuturi);
